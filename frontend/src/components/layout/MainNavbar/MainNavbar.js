7534c5020a9b53f7d216b253fd3e4b1dffe990e8
junho <kesu9251@naver.com>
2021-08-02 18:44:18 +0900

    템플릿 최소화
    기본 게시판을 제외한 나머지 부분 삭제.
    
    Signed-off-by: junho <kesu9251@naver.com>

--- frontend/src/components/layout/MainNavbar/MainNavbar.js
+++ frontend/src/components/layout/MainNavbar/MainNavbar.js
@@ -4,8 +4,6 @@ import classNames from "classnames";
 import { Container, Navbar } from "shards-react";
 
 import NavbarSearch from "./NavbarSearch";
-import NavbarNav from "./NavbarNav/NavbarNav";
-import NavbarToggle from "./NavbarToggle";
 
 const MainNavbar = ({ layout, stickyTop }) => {
   const classes = classNames(
@@ -19,8 +17,6 @@ const MainNavbar = ({ layout, stickyTop }) => {
       <Container className="p-0">
         <Navbar type="light" className="align-items-stretch flex-md-nowrap p-0">
           <NavbarSearch />
-          <NavbarNav />
-          <NavbarToggle />
         </Navbar>
       </Container>
     </div>
--- frontend/src/views/BlogPosts.js
+++ frontend/src/views/BlogPosts.js
@@ -31,39 +31,6 @@ class BlogPosts extends React.Component {
           body:
             "However venture pursuit he am mr cordial. Forming musical am hearing studied be luckily. But in for determine what would see...",
           date: "28 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/2.jpeg"),
-          category: "Travel",
-          categoryTheme: "info",
-          author: "James Jamerson",
-          authorAvatar: require("../images/avatars/2.jpg"),
-          title: "Off tears are day blind smile alone had ready",
-          body:
-            "Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my...",
-          date: "29 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/3.jpeg"),
-          category: "Technology",
-          categoryTheme: "royal-blue",
-          author: "Jimmy Jackson",
-          authorAvatar: require("../images/avatars/2.jpg"),
-          title: "Difficult in delivered extensive at direction",
-          body:
-            "Is at purse tried jokes china ready decay an. Small its shy way had woody downs power. To denoting admitted speaking learning my...",
-          date: "29 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/4.jpeg"),
-          category: "Business",
-          categoryTheme: "warning",
-          author: "John James",
-          authorAvatar: require("../images/avatars/3.jpg"),
-          title: "It so numerous if he may outlived disposal",
-          body:
-            "How but sons mrs lady when. Her especially are unpleasant out alteration continuing unreserved ready road market resolution...",
-          date: "29 February 2019"
         }
       ],
 
@@ -80,18 +47,6 @@ class BlogPosts extends React.Component {
           body:
             "Conviction up partiality as delightful is discovered. Yet jennings resolved disposed exertion you off. Left did fond drew fat head poor jet pan flying over...",
           date: "29 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/6.jpeg"),
-          category: "Business",
-          categoryTheme: "dark",
-          author: "John James",
-          authorAvatar: require("../images/avatars/1.jpg"),
-          title:
-            "Totally words widow one downs few age every seven if miss part by fact",
-          body:
-            "Discovered had get considered projection who favourable. Necessary up knowledge it tolerably. Unwilling departure education to admitted speaking...",
-          date: "29 February 2019"
         }
       ],
 
@@ -103,23 +58,6 @@ class BlogPosts extends React.Component {
           title: "Had denoting properly jointure which well books beyond",
           body:
             "In said to of poor full be post face snug. Introduced imprudence see say unpleasing devonshire acceptance son. Exeter longer wisdom work...",
-          date: "29 February 2019"
-        },
-        {
-          author: "John James",
-          authorAvatar: require("../images/avatars/2.jpg"),
-          title: "Husbands ask repeated resolved but laughter debating",
-          body:
-            "It abode words began enjoy years no do ﻿no. Tried spoil as heart visit blush or. Boy possible blessing sensible set but margaret interest. Off tears...",
-          date: "29 February 2019"
-        },
-        {
-          author: "John James",
-          authorAvatar: require("../images/avatars/3.jpg"),
-          title:
-            "Instantly gentleman contained belonging exquisite now direction",
-          body:
-            "West room at sent if year. Numerous indulged distance old law you. Total state as merit court green decay he. Steepest merit checking railway...",
           date: "29 February 2019"
         }
       ],
@@ -135,39 +73,6 @@ class BlogPosts extends React.Component {
           title: "Extremity so attending objection as engrossed",
           body:
             "Pursuit chamber as elderly amongst on. Distant however warrant farther to of. My justice wishing prudent waiting in be...",
-          date: "29 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/8.jpeg"),
-          author: "Chris Jamie",
-          authorUrl: "#",
-          category: "News",
-          categoryUrl: "#",
-          title: "Bed sincerity yet therefore forfeited his",
-          body:
-            "Speaking throwing breeding betrayed children my to. Me marianne no he horrible produced ye. Sufficient unpleasing and...",
-          date: "29 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/9.jpeg"),
-          author: "Monica Jordan",
-          authorUrl: "#",
-          category: "News",
-          categoryUrl: "#",
-          title: "Object remark lively all did feebly excuse our",
-          body:
-            "Morning prudent removal an letters by. On could my in order never it. Or excited certain sixteen it to parties colonel not seeing...",
-          date: "29 February 2019"
-        },
-        {
-          backgroundImage: require("../images/content-management/10.jpeg"),
-          author: "Monica Jordan",
-          authorUrl: "#",
-          category: "News",
-          categoryUrl: "#",
-          title: "His followed carriage proposal entrance",
-          body:
-            "For county now sister engage had season better had waited. Occasional mrs interested far expression directly as regard...",
           date: "29 February 2019"
         }
       ]
