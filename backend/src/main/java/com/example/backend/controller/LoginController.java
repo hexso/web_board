1854e872d9084a2b570d57c3144d14b41515b47a
junho <kesu9251@naver.com>
2021-10-04 15:19:45 +0900

    로그인, 로그아웃 세션할당 및 삭제 기능 구현
    Signed-off-by: junho <kesu9251@naver.com>

--- backend/src/main/java/com/example/backend/controller/LoginController.java
+++ backend/src/main/java/com/example/backend/controller/LoginController.java
@@ -3,12 +3,15 @@ package com.example.backend.controller;
 import com.example.backend.dto.LoginForm;
 import com.example.backend.model.User;
 import com.example.backend.service.LoginService;
+import com.example.backend.util.SessionConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api")
@@ -18,12 +21,21 @@ public class LoginController {
     private LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginForm loginForm) {
+    public ResponseEntity login(@RequestBody LoginForm loginForm, HttpSession session) {
         User user = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
         if (user == null) {
             return ResponseEntity.ok("fail");
         }
-        else
-            return ResponseEntity.ok(user);
+        session.setAttribute(SessionConstants.LOGIN_USER, user.getUserName());
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity logout(@RequestBody LoginForm loginForm, HttpSession session) {
+        if (session != null) {
+            session.invalidate();
+        }
+        return ResponseEntity.ok("ok");
     }
 }
---
+++ backend/src/main/java/com/example/backend/util/SessionConstants.java
@@ -0,0 +1,5 @@
+package com.example.backend.util;
+
+public interface SessionConstants {
+    String LOGIN_USER = "loginUser";
+}
